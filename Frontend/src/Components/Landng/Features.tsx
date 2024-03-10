dba3283ca1b651f47a95b41f13c17fc12f3e9256
sudonite <sudonite@gmail.com>
2024-03-10 03:00:38 +0100

    Add: Features

--- Frontend/src/Components/Landng/Features.tsx
+++ Frontend/src/Components/Landng/Features.tsx
@@ -1,5 +1,70 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/Components/UI/Card";
+
+import { FaCodeBranch } from "react-icons/fa6";
+
+interface FeatureProps {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
+
+const features: FeatureProps[] = [
+  {
+    icon: <FaCodeBranch />,
+    title: "Accesibility",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quas provident cum",
+  },
+  {
+    icon: <FaCodeBranch />,
+    title: "Community",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quas provident cum",
+  },
+  {
+    icon: <FaCodeBranch />,
+    title: "Scalability",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quas provident cum",
+  },
+  {
+    icon: <FaCodeBranch />,
+    title: "Gamification",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quas provident cum",
+  },
+];
+
 const Features = () => {
-  return <p>Features</p>;
+  return (
+    <section className="container mx-auto text-center py-32">
+      <h2 className="text-4xl font-bold ">
+        Many
+        <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+          {" "}
+          Great Features{" "}
+        </span>
+      </h2>
+      <p className="w-3/4 mx-auto mt-4 mb-8 text-xl text-muted-foreground">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+        dolor pariatur sit!
+      </p>
+
+      <div className="grid grid-cols-4 gap-8">
+        {features.map(({ icon, title, description }: FeatureProps) => (
+          <Card key={title} className="bg-muted/50">
+            <CardHeader>
+              <CardTitle className="grid gap-4 place-items-center">
+                {icon}
+                {title}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>{description}</CardContent>
+          </Card>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Features;
