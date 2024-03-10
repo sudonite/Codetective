73591d09b15b6c198462ebeb93f2b1c3af797919
sudonite <sudonite@gmail.com>
2024-03-10 03:02:15 +0100

    Add: HowItWorks

--- Frontend/src/Components/Landng/HowItWorks.tsx
+++ Frontend/src/Components/Landng/HowItWorks.tsx
@@ -1,5 +1,78 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/Components/UI/Card";
+
+import { FaCodeBranch } from "react-icons/fa6";
+
+interface ServiceProps {
+  title: string;
+  description: string;
+  icon: JSX.Element;
+}
+
+const serviceList: ServiceProps[] = [
+  {
+    title: "Code Collaboration",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
+    icon: <FaCodeBranch />,
+  },
+  {
+    title: "Project Management",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
+    icon: <FaCodeBranch />,
+  },
+  {
+    title: "Task Automation",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
+    icon: <FaCodeBranch />,
+  },
+];
+
 const HowItWorks = () => {
-  return <p>HowItWorks</p>;
+  return (
+    <section className="container mx-auto py-24 sm:py-32">
+      <div className="grid lg:grid-cols-[1fr,1fr] gap-8 place-items-center">
+        <div>
+          <h2 className="text-3xl md:text-4xl font-bold">
+            How It
+            <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+              {" "}
+              Works
+            </span>
+          </h2>
+
+          <p className="text-muted-foreground text-xl mt-4 mb-8 ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+            dolor.
+          </p>
+
+          <div className="flex flex-col gap-8">
+            {serviceList.map(({ icon, title, description }: ServiceProps) => (
+              <Card key={title}>
+                <CardHeader className="space-y-1 flex md:flex-row justify-start items-start gap-4">
+                  <div className="mt-1 bg-primary/20 p-2 rounded-2xl">
+                    {icon}
+                  </div>
+                  <div>
+                    <CardTitle>{title}</CardTitle>
+                    <CardDescription className="text-md mt-2">
+                      {description}
+                    </CardDescription>
+                  </div>
+                </CardHeader>
+              </Card>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HowItWorks;
