d35de97ae87f0c25c3122afca7d2d4bf9cadb444
sudonite <sudonite@gmail.com>
2024-03-10 03:15:17 +0100

    Add: Newsletter

--- Frontend/src/Components/Landng/Newsletter.tsx
+++ Frontend/src/Components/Landng/Newsletter.tsx
@@ -1,5 +1,43 @@
+import { Button } from "@/Components/UI/Button";
+import { Input } from "@/Components/UI/Input";
+
 const Newsletter = () => {
-  return <p>Newsletter</p>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Subscribed!");
+  };
+
+  return (
+    <section id="newsletter">
+      <hr className="w-11/12 mx-auto" />
+
+      <div className="container mx-auto py-24 sm:py-32">
+        <h3 className="text-center text-4xl md:text-5xl font-bold">
+          Join Our Daily{" "}
+          <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+            Newsletter
+          </span>
+        </h3>
+        <p className="text-xl text-muted-foreground text-center mt-4 mb-8">
+          Lorem ipsum dolor sit amet consectetur.
+        </p>
+
+        <form
+          className="flex flex-col w-full md:flex-row md:w-6/12 lg:w-4/12 mx-auto gap-4 md:gap-2"
+          onSubmit={handleSubmit}
+        >
+          <Input
+            placeholder="example@mail.com"
+            className="bg-muted/50 dark:bg-muted/80 "
+            aria-label="email"
+          />
+          <Button>Subscribe</Button>
+        </form>
+      </div>
+
+      <hr className="w-11/12 mx-auto" />
+    </section>
+  );
 };
 
 export default Newsletter;
