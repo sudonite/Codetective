2c2f7cde4ad95993227ae9cc989e51a3a839e646
sudonite <sudonite@gmail.com>
2024-04-15 06:48:13 +0200

    Fix: FileArea Icons

--- Frontend/src/Components/Dashboard/FileIcon.tsx
+++ Frontend/src/Components/Dashboard/FileIcon.tsx
@@ -3,9 +3,11 @@ import { FaFile } from "react-icons/fa";
 
 const FileIcon = ({ extension }: { extension: string }) => {
   switch (extension) {
-    case "cpp":
+    case ".cpp":
       return <TbBrandCpp className="bg-primary/30 p-2 rounded-lg w-10 h-10" />;
-    case "c":
+    case ".cc":
+      return <TbBrandCpp className="bg-primary/30 p-2 rounded-lg w-10 h-10" />;
+    case ".c":
       return <TbBrandCpp className="bg-primary/30 p-2 rounded-lg w-10 h-10" />;
     default:
       return <FaFile className="bg-primary/30 p-2 rounded-lg w-10 h-10" />;
